663e8ce6a4dfe0288c302864e9abd079b05c8efc
narwhal04 <q.quek7@gmail.com>
2022-03-31 22:22:57 +0800

    functioning code

--- arrays.go
+++ arrays.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"fmt"
+)
+
+var (
+	winList = [6]string{"Windows xp", "Linux 1", "Raspbian Teensy", "MacOS", "iOS", "Google Android"}
+)
+
 func main() {
-	//Insert Code here
+
+	winList[0] = "windows10"
+	winList[1] = "linux 16.04"
+	winList[2] = "Raspbian Buster"
+
+	fmt.Println(winList)
+
+	newList := [9]string{}
+
+	for i := 0; i < len(winList); i++ {
+		newList[i] = winList[i]
+	}
+
+	newList[6] = "Ubuntu"
+	newList[7] = "MS-Dos"
+	newList[8] = "Solaris"
+
+	fmt.Println(newList)
+	fmt.Println(newList[6:])
+
 }
